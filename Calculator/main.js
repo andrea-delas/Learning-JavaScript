8dd67e34f59aeae6e7fbb337b8eb9a01e5ef7023
Andrea Delaš <andrea.kramar28@gmail.com>
2019-03-11 12:27:02 +0100

    putting the result into the browser

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,6 +1,3 @@
-
-
-
 function calc () {
 	var a = parseInt(document.querySelector("#value1").value);
 	var b = parseInt(document.querySelector("#value2").value);
@@ -17,5 +14,5 @@ function calc () {
 		calculate = a * b;
 	}
 
-	console.log(calculate);
+	document.querySelector("#result").innerHTML = calculate;
 }

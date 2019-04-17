0c6d648c29b8518f3b9dd71c7e5da8f125de9d22
Andrea Delaš <andrea.kramar28@gmail.com>
2019-04-17 19:52:30 +0200

    scoping

---
+++ JavaScript behind the scenes/Scoping and the Scope Chain/script.js
@@ -0,0 +1,43 @@
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+//First scoping example
+
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);  // result Hello!Hi!Hey!
+    }
+}
+
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    //console.log(c); - // c is not defined
+    console.log(a+d);
+}
